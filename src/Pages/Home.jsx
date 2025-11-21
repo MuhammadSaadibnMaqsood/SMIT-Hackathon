8efa589d18bb8fb27220d9a14bc6a504af6804b1
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-11-21 14:04:15 -0800

    Initialize project and wrote some boiler plate for today hackathon

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="bg-amber-400 h-screen w-full flex items-center justify-center">
+
+    <div className="h-[50%] bg-red-500 w-[50%]">
+
+    </div>
+  </div>;
+};
+
+export default Home;
